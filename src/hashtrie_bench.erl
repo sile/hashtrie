4ec1f277810a8a2c87d4a0bac3c4f5dc4902c237
Takeru Ohta <phjgt308@gmail.com>
2014-07-03 06:37:00 +0900

    ベンチマークモジュールを追加

--- src/hashtrie_bench.erl
+++ src/hashtrie_bench.erl
@@ -5,130 +5,136 @@
 %% Exported API
 %%----------------------------------------------------------------------------------------------------------------------
 -export([
-         %% TODO: bench/1
+         bench/0,
+         print_bench/0
         ]).
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported Functions
 %%----------------------------------------------------------------------------------------------------------------------
+-spec bench() -> [PerResult] when
+      PerResult :: {KeyType, Method, Order, Case, module(), ElapsedMicroSeconds},
+      KeyType   :: integer | binary | tuple,
+      Order     :: sorted | random,
+      Case      :: {1, 100000}  % {Size, LoopCount}
+                 | {10, 10000}
+                 | {100, 1000}
+                 | {10000, 10}
+                 | {100000, 1},
+      Method    :: store | successful_find | successful_erase,
+      ElapsedMicroSeconds :: non_neg_integer().
+bench() ->
+    Result0 = do_bench(integer, fun generate_sorted_integer_entries/1),
+    Result1 = do_bench(binary, fun generate_sorted_binary_entries/1),
+    Result2 = do_bench(tuple, fun generate_sorted_tuple_entries/1),
+    lists:sort(Result0 ++ Result1 ++ Result2).
 
--export([gen_int_entries/2, gen_str_entries/1]).
--export([store_entries/2, store_entries/3, store_loop/4, find_entries/3]).
--export([store_time/2, store_time/3, find_time/3]).
--export([store_memory/2]).
+-spec print_bench() -> ok.
+print_bench() ->
+    io:format("\n"),
+    lists:foreach(
+      fun ({KeyType, Method, Order, {Size, LoopCount}, Module, Elapsed}) ->
+              io:format("~s\t~s\t~s\t~p\t~p\t~10s\t~p\n",
+                        [KeyType, Method, Order, Size, LoopCount, Module, Elapsed])
+      end,
+      bench()).
 
-gen_int_entries(Start,End) ->
-    lists:map(fun (X) -> {X,X} end, lists:seq(Start,End-1)).
+%%----------------------------------------------------------------------------------------------------------------------
+%% Internal Functions
+%%----------------------------------------------------------------------------------------------------------------------
+do_bench(KeyType, GenerateFun) ->
+    lists:flatmap(
+      fun ({Size, LoopCount}) ->
+              io:format(standard_error, "# [~s] ~p, ~p\n", [KeyType, Size, LoopCount]),
 
-gen_str_entries(Filepath) ->
-    {ok, In} = file:open(Filepath, [read]),
-    read_lines(In,[],0).
+              Sorted = GenerateFun(Size),
+              Random = shuffle(Sorted),
+              lists:flatmap(
+                fun (MapModule) ->
+                        Empty = (method(MapModule, from_list))([]),
+                        Map   = (method(MapModule, from_list))(Sorted),
+                        [
+                         {KeyType, store, sorted, {Size, LoopCount}, MapModule, times(LoopCount, Sorted, Empty, method(MapModule, store))},
+                         {KeyType, store, random, {Size, LoopCount}, MapModule, times(LoopCount, Random, Empty, method(MapModule, store))},
+                         {KeyType, successful_find, sorted, {Size, LoopCount}, MapModule, times(LoopCount, Sorted, Map, method(MapModule, find))},
+                         {KeyType, successful_find, random, {Size, LoopCount}, MapModule, times(LoopCount, Random, Map, method(MapModule, find))},
+                         {KeyType, successful_erase, sorted, {Size, LoopCount}, MapModule, times(LoopCount, Sorted, Map, method(MapModule, erase))},
+                         {KeyType, successful_erase, random, {Size, LoopCount}, MapModule, times(LoopCount, Random, Map, method(MapModule, erase))}
+                        ]
+                end,
+                map_modules())
+      end,
+      case_list()).
 
-read_lines(In, Lines, LineNum) ->
-    case file:read_line(In) of
-        {ok, Line} -> 
-            BinaryLine = list_to_binary(string:strip(Line,right,$\n)),
-            read_lines(In, [{BinaryLine,LineNum}|Lines], LineNum+1);
-        eof -> lists:reverse(Lines)
+case_list() ->
+    [
+     {1, 100000},
+     {10, 10000},
+     {100, 1000},
+     {1000, 100},
+     {10000, 10},
+     {100000, 1}
+    ].
+
+-spec map_modules() -> [module()].
+map_modules() ->
+    [dict, gb_trees, hashtrie].
+
+-spec method(module(), Method) -> function() when
+      Method :: from_list | store | find | erase.
+method(dict, from_list) -> fun dict:from_list/1;
+method(dict, store)     -> fun ({K, V}, M) -> dict:store(K, V, M) end;
+method(dict, find)      -> fun ({K, _}, M) -> _ = dict:find(K, M), M end;
+method(dict, erase)     -> fun dict:erase/2;
+
+method(gb_trees, from_list) -> fun gb_trees:from_orddict/1;
+method(gb_trees, store)     -> fun ({K, V}, M) -> gb_trees:enter(K, V, M) end;
+method(gb_trees, find)      -> fun ({K, _}, M) -> _ = gb_trees:lookup(K, M), M end;
+method(gb_trees, erase)     -> fun gb_trees:delete_any/2;
+
+method(hashtrie, from_list) -> fun hashtrie:from_list/1;
+method(hashtrie, store)     -> fun ({K, V}, M) -> hashtrie:store(K, V, M) end;
+method(hashtrie, find)      -> fun ({K, _}, M) -> _ = hashtrie:find(K, M), M end;
+method(hashtrie, erase)     -> fun hashtrie:erase/2.
+
+times(LoopCount, InputData, Map, Fun) ->
+    true = garbage_collect(),
+    {Elapsed, _} =
+        timer:tc(
+          fun () ->
+                  loop(LoopCount, InputData, Map, Fun)
+          end),
+    Elapsed.
+
+loop(0, _, _, _) ->
+    ok;
+loop(LoopCount, InputData, Map, Fun) ->
+    _ = lists:foldl(fun (X, Acc) -> Fun(X, Acc) end, Map, InputData),
+    loop(LoopCount - 1, InputData, Map, Fun).
+
+generate_sorted_integer_entries(Count) ->
+    [{X, X} || X <- lists:seq(0, Count - 1)].
+
+generate_sorted_binary_entries(Count) ->
+    generate_sorted_binary_entries(Count, []).
+
+generate_sorted_binary_entries(Count, Acc) ->
+    List0 = 
+        [begin
+             X = crypto:rand_bytes(random:uniform(100)),
+             {X, X}
+         end || _ <- lists:seq(0, Count - 1)],
+    List1 = lists:usort(List0),
+    List2 = lists:umerge(Acc, List1),
+    case (length(Acc) + Count) - length(List2) of
+        0 -> List2;
+        N -> generate_sorted_binary_entries(N, List2)
     end.
 
-store_entries(Entries, InitObj, StoreFn) ->
-    lists:foldl(fun ({K,V},T) -> StoreFn(K, V, T) end,
-                InitObj,
-                Entries).
+generate_sorted_tuple_entries(Count) ->
+    lists:sort(
+      lists:zip(shuffle(generate_sorted_integer_entries(Count)),
+                shuffle(generate_sorted_binary_entries(Count)))).
 
-store_entries(Entries, hashtrie) ->
-    hashtrie_test:store_entries(Entries, hashtrie:new(), fun hashtrie:store/3);
-store_entries(Entries, dict) ->
-    hashtrie_test:store_entries(Entries, dict:new(), fun dict:store/3);
-store_entries(Entries, gb_trees) ->
-    hashtrie_test:store_entries(Entries, gb_trees:empty(), fun gb_trees:insert/3).
-
-store_loop(_, _, _, 0) -> done;
-store_loop(Entries, InitObj, StoreFn, LoopCount) ->
-    store_entries(Entries, InitObj, StoreFn),
-    store_loop(Entries, InitObj, StoreFn, LoopCount-1).
-
-find_entries(Entries, Obj, FindFn) ->
-    lists:foldl(fun ({K,_}, Cnt) -> 
-                        case FindFn(K, Obj) of
-                            {_,_} -> Cnt+1;
-                            _     -> Cnt
-                        end
-                end,
-                0,
-                Entries).
-
-extract_time({Time,_}) -> Time.
-
-store_time(Entries, MapType) ->
-    extract_time(timer:tc(?MODULE, store_entries, [Entries, MapType])). 
-
-store_time(Entries, hashtrie, LoopCount) ->
-    extract_time(timer:tc(?MODULE, store_loop, 
-                          [Entries, hashtrie:new(), fun hashtrie:store/3, LoopCount]));
-store_time(Entries, dict, LoopCount) ->
-    extract_time(timer:tc(?MODULE, store_loop, 
-                          [Entries, dict:new(), fun dict:store/3, LoopCount]));
-store_time(Entries, gb_trees, LoopCount) ->
-    extract_time(timer:tc(?MODULE, store_loop, 
-                          [Entries, gb_trees:empty(), fun gb_trees:insert/3, LoopCount])).
-
-find_time(Entries, Trie, hashtrie) ->
-    extract_time(timer:tc(?MODULE, find_entries,
-                          [Entries, Trie, fun hashtrie:find/2]));
-find_time(Entries, Dict, dict) ->
-    extract_time(timer:tc(?MODULE, find_entries,
-                          [Entries, Dict, fun dict:find/2]));
-find_time(Entries, Tree, gb_trees) ->
-    extract_time(timer:tc(?MODULE, find_entries,
-                          [Entries, Tree, fun gb_trees:lookup/2])).
-
-store_memory(Entries, MapType) ->
-    erlang:garbage_collect(),
-    Before = erlang:memory(processes),
-    _Map = store_entries(Entries, MapType),
-    After = erlang:memory(processes),
-    After-Before.
-
-%% Num10 = hashtrie_test:gen_int_entries(0, 10).
-%% Num100 = hashtrie_test:gen_int_entries(0, 100).
-%% Num100000 = hashtrie_test:gen_int_entries(0, 100000).
-%% Str100000 = hashtrie_test:gen_str_entries("words.100000").
-%% Str300000 = hashtrie_test:gen_str_entries("words").
-
-%% hashtrie_test:store_time(Num10, hashtrie, 10000).
-%% hashtrie_test:store_time(Num10, dict, 10000).    
-%% hashtrie_test:store_time(Num10, gb_trees, 10000).
-
-%% hashtrie_test:store_time(Num100, hashtrie, 1000).
-%% hashtrie_test:store_time(Num100, dict, 1000).    
-%% hashtrie_test:store_time(Num100, gb_trees, 1000).
-
-%% hashtrie_test:store_time(Num100000, hashtrie).
-%% hashtrie_test:store_time(Num100000, dict).    
-%% hashtrie_test:store_time(Num100000, gb_trees).
-
-%% hashtrie_test:store_time(Str100000, hashtrie).
-%% hashtrie_test:store_time(Str100000, dict).
-%% hashtrie_test:store_time(Str100000, gb_trees).
-
-%% hashtrie_test:store_memory(Str300000, hashtrie).
-%% hashtrie_test:store_memory(Str300000, dict).
-%% hashtrie_test:store_memory(Str300000, gb_trees).
-
-%% Tn = hashtrie_test:store_entries(Num100000, hashtrie), done.
-%% Dn = hashtrie_test:store_entries(Num100000, dict), done.
-%% Gn = hashtrie_test:store_entries(Num100000, gb_trees), done.
-
-%% Ts = hashtrie_test:store_entries(Str100000, hashtrie), done.
-%% Ds = hashtrie_test:store_entries(Str100000, dict), done.
-%% Gs = hashtrie_test:store_entries(Str100000, gb_trees), done.
-
-%% hashtrie_test:find_time(Num100000, Tn, hashtrie).
-%% hashtrie_test:find_time(Num100000, Dn, dict).    
-%% hashtrie_test:find_time(Num100000, Gn, gb_trees).
-
-%% hashtrie_test:find_time(Str300000, Ts, hashtrie).
-%% hashtrie_test:find_time(Str300000, Ds, dict).    
-%% hashtrie_test:find_time(Str300000, Gs, gb_trees).
+shuffle(List) ->
+    [X||{_,X} <- lists:keysort(1, [{random:uniform(), N} || N <- List])].
